151c6da25981cdca822698652c75a716276c7f42
Ryu Taehwan <yth3260@gmail.com>
2018-01-25 16:58:23 +0900

    get_top100_list()함수 구현 및 main 함수 정리

--- main.py
+++ main.py
@@ -1,32 +1,4 @@
-from bs4 import BeautifulSoup
-import re
 from utils import get_top100_list
-# source = open('melon.html', 'rt').read()
-# soup = BeautifulSoup(source, 'lxml')
-
-
-# for index, div in enumerate(soup.find_all('div', class_='rank01')):
-#     title = div.find('a').text
-#     print(index+1, title)
-# result = []
-# for tr in soup.find_all('tr', class_='lst50'):
-#     rank = tr.find('span',class_='rank').text
-#     title = tr.find('div',class_='rank01').find('a').text
-#     artist = tr.find('div', class_='rank02').find('a').text
-#     album = tr.find('div', class_='rank03').find('a').text
-#     url_image_cover = tr.find('a',class_='image_typeAll').find('img')['src']
-#     p = re.compile(r'(.*\..*?)')
-#     url_image_cover = re.search(p, url_image_cover).group(1)
-#     result.append({
-#         'rank' : rank,
-#         'title' : title,
-#         'url_img_cover' : url_image_cover,
-#         'artist' : artist,
-#         'album' : album,
-#     })
-#
-# for item in result:
-#     print(item)
 
 if __name__=='__main__':
     result = get_top100_list()
--- utils.py
+++ utils.py
@@ -7,8 +7,7 @@ def get_top100_list(refresh_html=False):
     실시간 차트 1~100위의 리스트 반환
     :return:
     """
-    url_chart_realtime_50 = 'https://www.melon.com/chart/index.htm'
-    url_chart_realtime_100 = 'https://www.melon.com/chart/index.htm#params%5Bidx%5D=51'
+    url_chart_realtime = 'https://www.melon.com/chart/index.htm'
     # 프로젝트가 실행되는 '파일'의 path
     # /utils
     path_module = os.path.abspath(__name__)
@@ -33,7 +32,7 @@ def get_top100_list(refresh_html=False):
     # 파일이 있는 경우를 검사 후 로직 실행
     # refresh_html 매개변수가 true일 경우, 무조건 새로 파일을 다운받도록 함 -> 존재하지 않을때 덮어써 또는 매개변수 true일때
     if not os.path.exists(file_path) or refresh_html:
-        response = requests.get(url_chart_realtime_50)
+        response = requests.get(url_chart_realtime)
         source = response.text
         with open(file_path, 'wt') as f:
             f.write(source)
@@ -41,31 +40,28 @@ def get_top100_list(refresh_html=False):
         print(f'"{file_path}" file is already exists!!')
 
 
-    # try-except문, xt로 파일이 이미 존재하는지 확인
-    # file_path_100 = os.path.join(path_data_dir, 'chart_realtime_100.html')
+    # # try-except문, xt로 파일이 이미 존재하는지 확인
+    # file_path = os.path.join(path_data_dir, 'chart_realtime.html')
     # try:
     #     word = 'wt' if refresh_html else 'xt'
-    #     with open(file_path_100, word) as f:
-    #         response = requests.get(url_chart_realtime_100)
+    #     with open(file_path, word) as f:
+    #         response = requests.get(url_chart_realtime)
     #         source = response.text
     #         f.write(source)
     # except FileExistsError as e:
-    #     print(f'"{file_path_100}" file is already exists!!')
-
+    #     print(f'"{file_path}" file is already exists!!')
 
     result = []
 
-    # 50위까지
-
     source = open(file_path, 'rt').read()
     soup = BeautifulSoup(source, 'lxml')
-    for tr in soup.find_all('tr', class_='lst50'):
+    for tr in soup.find_all('tr', class_=['lst50','lst100']): # 멀티 class 지정
         rank = tr.find('span', class_='rank').text
         title = tr.find('div', class_='rank01').find('a').text
         artist = tr.find('div', class_='rank02').find('a').text
         album = tr.find('div', class_='rank03').find('a').text
-        url_image_cover = tr.find('a', class_='image_typeAll').find('img')['src']
-        p = re.compile(r'(.*\..*?)')
+        url_image_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
+        p = re.compile(r'(.*\..*?)/')
         url_image_cover = re.search(p, url_image_cover).group(1)
         result.append({
             'rank': rank,
